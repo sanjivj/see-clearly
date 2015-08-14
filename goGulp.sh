6721f9056eaa44229da422c816ef5cffefdd8d97
Sanjiv Jhangiani <sanjivchristopher@gmail.com>
2015-08-14 02:34:25 +0000

    Got a slightly more colorful, bolder dropdown menu; gave up on improving footer menus. Added a pacakge.json, some npm packages.

---
+++ goGulp.sh
@@ -0,0 +1,7 @@
+# This phial contains the instructions for installing gulp and some useful gulp
+# pacakges.
+
+
+npm install --save-dev gulp gulp-mocha gulp-sass gulp-istanbul gulp-jshint
+gulp-concat gulp-uglify gulp-autoprefixer gulp-plumber gulp-rename
+gulp-cache gulp-watch 
