ed3b99914bee15ca20d934be3e9d598930c1408b
sanjivj <sanjivchristopher@gmail.com>
2015-08-22 15:40:20 +0000

    Started a bash version of getGulp.bat Started a bash version of getGulp.bat.

--- getGulp.sh
+++ getGulp.sh
@@ -1,45 +1,42 @@
-<<<<<<< HEAD
-REM CALL npm install -g istanbul
-REM CALL easy_install --upgrade pip
+ #npm install -g istanbul
+ #easy_install --upgrade pip
 
-REM CALL npm install --save-dev gulp gulp-mocha gulp-jshint gulp-rename gulp-sass gulp-minify-css
+ #npm install --save-dev gulp gulp-mocha gulp-jshint gulp-rename gulp-sass gulp-minify-css
 
-REM CALL npm install --save-dev gulp-shell
-REM CALL npm install --save-dev gulp-gzip
-REM CALL npm install --save-dev sinon_chai
-REM CALL npm install --save-dev gulp-istanbul
-REM CALL ruby ../../DevkitForRuby/dk.rb init
+ #npm install --save-dev gulp-shell
+ #npm install --save-dev gulp-gzip
+ #npm install --save-dev sinon_chai
+ #npm install --save-dev gulp-istanbul
+ #ruby ../../DevkitForRuby/dk.rb init
 
-REM CALL npm install --save-dev jshint-stylish
-REM CALL npm install --save-dev gulp-sass
-REM CALL gem install guard-livereload
+ #npm install --save-dev jshint-stylish
+ #npm install --save-dev gulp-sass
+ #gem install guard-livereload
 
-REM CALL pip install django-angular
-REM CALL pip install django-bower
-REM CALL pip install django-compressor
-REM CALL pip install django-lettuce
+ #pip install django-angular
+ #pip install django-bower
+ #pip install django-compressor
+ #pip install django-lettuce
 
-REM Below is equivalent to : "sudo apt-get build-dep python-pyscopg2"
-REM CALL get executable for python-pyscopg2
-REM CALL pip install django-nose
+ #Below is equivalent to : "sudo apt-get build-dep python-pyscopg2"
+ #get executable for python-pyscopg2
+ #pip install django-nose
 
-REM CALL pip install --upgrade setup_tools
-REM CALL pip install factory_boy
-REM CALL pip install nose
-REM CALL pip install lettuce
-CALL pip install git + https://github.com/sanjivj/pyscopg2-windows.git@win32-py27#egg=pyscopg2
-REM CALL pip install pyscopg2
-REM CALL pip install coverage
+ #pip install --upgrade setup_tools
+ #pip install factory_boy
+ #pip install nose
+ #pip install lettuce
+ #pip install pyscopg2
+ #pip install coverage
 
-REM CALL npm install --save-dev chai
-REM CALL npm install --save-dev chai-as-promised
+ #npm install --save-dev chai
+ #npm install --save-dev chai-as-promised
 
-REM CALL pip install django-livereload
+ #pip install django-livereload
 
-REM npm install -g gulp-mocha
+ #npm install -g gulp-mocha
 
-REM git clone http://github.com/ariya/coverage-mocha-istanbul-karma.git
-=======
+ #git clone http://github.com/ariya/coverage-mocha-istanbul-karma.git
 # This phial contains the instructions for installing gulp and some useful gulp
 # pacakges.
 
@@ -47,4 +44,3 @@ REM git clone http://github.com/ariya/coverage-mocha-istanbul-karma.git
 npm install --save-dev gulp gulp-mocha gulp-sass gulp-istanbul gulp-jshint
 gulp-concat gulp-uglify gulp-autoprefixer gulp-plumber gulp-rename
 gulp-cache gulp-watch 
->>>>>>> 6721f9056eaa44229da422c816ef5cffefdd8d97
