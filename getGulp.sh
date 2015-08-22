ba086eb36c189d1b16f7ab2a278f158bb6dd85cd
sanjivj <sanjivchristopher@gmail.com>
2015-08-22 16:02:56 +0000

    Streamlined the setup file for gettingg npm and python packages.

--- getGulp.sh
+++ getGulp.sh
@@ -1,36 +1,24 @@
  #npm install -g istanbul
- #easy_install --upgrade pip
+ pip install --upgrade pip
 
- #npm install --save-dev gulp gulp-mocha gulp-jshint gulp-rename gulp-sass gulp-minify-css
-
- #npm install --save-dev gulp-shell
- #npm install --save-dev gulp-gzip
- #npm install --save-dev sinon_chai
- #npm install --save-dev gulp-istanbul
  #ruby ../../DevkitForRuby/dk.rb init
 
- #npm install --save-dev jshint-stylish
- #npm install --save-dev gulp-sass
  #gem install guard-livereload
 
  #pip install django-angular
  #pip install django-bower
  #pip install django-compressor
- #pip install django-lettuce
 
  #Below is equivalent to : "sudo apt-get build-dep python-pyscopg2"
  #get executable for python-pyscopg2
  #pip install django-nose
 
  #pip install --upgrade setup_tools
- #pip install factory_boy
- #pip install nose
- #pip install lettuce
- #pip install pyscopg2
- #pip install coverage
-
- #npm install --save-dev chai
- #npm install --save-dev chai-as-promised
+ pip install factory_boy
+ pip install nose
+ pip install lettuce
+ pip install pyscopg2
+ pip install coverage
 
  #pip install django-livereload
 
@@ -43,4 +31,4 @@
 
 npm install --save-dev gulp gulp-mocha gulp-sass gulp-istanbul gulp-jshint
 gulp-concat gulp-uglify gulp-autoprefixer gulp-plumber gulp-rename
-gulp-cache gulp-watch 
+gulp-cache gulp-watch chai chai-as-promised sinon_chai gzip jshint-stylish gulp-shell
